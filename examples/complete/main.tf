39582b29a4665b9f469f08dd3d6ef04939e45c8c
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-01-06 09:01:48 +0100

    chore: update complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -24,6 +24,8 @@ resource "azurerm_virtual_network" "hub" {
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   address_space       = ["10.0.0.0/16"]
+
+  tags = local.tags
 }
 
 resource "azurerm_network_security_group" "this" {
