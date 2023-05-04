5f0ad9359ae15254794840b856816500df5ac9d1
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-05-04 11:12:45 +0200

    feat(nsg): create network security group (#26)
    * feat(nsg): create network security group
    Fixes #24
    
    * chore: update basic example
    
    * refactor: reorder

---
+++ modules/nsg/variables.tf
@@ -0,0 +1,43 @@
+variable "nsg_name" {
+  description = "The name of this network security group."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group to create the resources in."
+  type        = string
+}
+
+variable "location" {
+  description = "The location to create the resources in."
+  type        = string
+}
+
+variable "security_rules" {
+  description = "A list of security rules to configure for this network security group."
+
+  type = list(object({
+    name                   = string
+    destination_port_range = string
+    direction              = string
+    priority               = number
+
+    # The following default values are taken from Azure Portal
+
+    description = optional(string, "")
+
+    access   = optional(string, "Allow")
+    protocol = optional(string, "*")
+
+    source_address_prefix      = optional(string, "*")
+    destination_address_prefix = optional(string, "*")
+  }))
+
+  default = []
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
+}
