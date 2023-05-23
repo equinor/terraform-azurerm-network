754bfd993a5f8c8d6ab233a57bcf2b3b10f5f9d0
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-05-23 09:45:02 +0200

    feat(nsg): create subnet and subnet nat association

--- modules/nat/main.tf
+++ modules/nat/main.tf
@@ -1,3 +1,10 @@
+resource "azurerm_subnet" "this" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_nat_gateway" "this" {
   name                = var.gateway_name
   resource_group_name = var.resource_group_name
@@ -5,6 +12,11 @@ resource "azurerm_nat_gateway" "this" {
   sku_name            = "Standard"
 
   tags = var.tags
+}
+
+resource "azurerm_subnet_nat_gateway_association" "this" {
+  subnet_id      = azurerm_subnet.this.id
+  nat_gateway_id = azurerm_nat_gateway.this.id
 }
 
 resource "azurerm_nat_gateway_public_ip_association" "this" {
