86c1d28ee9ab569c991dc2060c25a46621eab295
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-09-28 07:43:38 +0200

    refactor!: simplify DDoS Protection plan config

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ resource "azurerm_virtual_network" "this" {
   dns_servers         = var.dns_servers
 
   dynamic "ddos_protection_plan" {
-    for_each = var.ddos_protection_plan != null ? [0] : []
+    for_each = var.ddos_protection_plan_id != null ? [0] : []
 
     content {
-      id     = var.ddos_protection_plan["id"]
-      enable = var.ddos_protection_plan["enable"]
+      id     = var.ddos_protection_plan_id
+      enable = var.ddos_protection_plan_enabled
     }
   }
 
